0c87d67a190f2b1854125b547af0fa55b56e52ea
Nick Hale <nicholas.hale@gmail.com>
2018-08-09 10:48:52 +0100

    Updated slot Book method.

--- book/main.go
+++ book/main.go
@@ -1,73 +1,85 @@
- package main
+package main
 
- import (
-	 "context"
-	 "encoding/json"
-	 "errors"
-	 "log"
-	 "net/http"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
 
-	 "github.com/go-kit/kit/endpoint"
-	 httptransport "github.com/go-kit/kit/transport/http"
- )
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
 
- type Slot interface {
-	 Book(context.Context, bookRequest) (bookResponse, error)
- }
+type Slot interface {
+	Book(context.Context, bookRequest) (bookResponse, error)
+}
 
- type slot struct{}
+type slot struct{}
 
- func (slot) Book(_ context.Context, b bookRequest) (bookResponse, error) {
-	 br := bookResponse{b.Hour + ":" + b.Min + " 21/08/2018", b.Court, ""}
-	 return br, nil
- }
+func (slot) Book(_ context.Context, b bookRequest) (bookResponse, error) {
+	// read environment variables defining tynemouth squash url
+	// parse 
+	// submit
+	if b.Min == "" {
+		return bookResponse{"", "", "Min missing"}, errors.New("Empty min parameter")
+	}
+	br := bookResponse{b.Hour + ":" + b.Min + " 21/08/2018", b.Court, ""}
+	return br, nil
+}
 
- var ErrEmpty = errors.New("slot error")
+type bookRequest struct {
+	Days     string `json:"days"`
+	Court    string `json:"court"`
+	Hour     string `json:"hour"`
+	Min      string `json:"min"`
+	TimeSlot string `json:"timeslot"`
+}
 
- type bookRequest struct {
-	 Days		string `json:"days"`
-	 Court		string `json:"court"`
-	 Hour		string `json:"hour"`
-	 Min		string `json:"min"`
-	 TimeSlot	string `json:"timeslot"`
- }
+type bookResponse struct {
+	Time  string `json:"time"`
+	Court string `json:"court"`
+	Err   string `json:"err,omitempty"`
+}
 
- type bookResponse struct {
-	 Time	string `json:"time"`
-	 Court	string `json:"court"`
-	 Err	string `json:"err,omitempty"`
- }
+func makeBookEndpoint(svc Slot) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(bookRequest)
+		br, err := svc.Book(ctx, req)
+		if err != nil {
+			return bookResponse{br.Time, br.Court, err.Error()}, nil
+		}
+		return br, nil
+	}
+}
 
- func makeBookEndpoint(svc Slot) endpoint.Endpoint {
-	 return func(ctx context.Context, request interface{}) (interface{}, error) {
-		 req := request.(bookRequest)
-		 br, _ := svc.Book(ctx, req)
-		 return br, nil
-	 }
- }
+func main() {
+	svc := slot{}
 
- func main() {
-	 svc := slot{}
+	bookHandler := httptransport.NewServer(
+		makeBookEndpoint(svc),
+		decodeBookRequest,
+		encodeResponse,
+	)
 
-	 bookHandler := httptransport.NewServer(
-		 makeBookEndpoint(svc),
-		 decodeBookRequest,
-		 encodeResponse,
-	 )
+	http.Handle("/book", bookHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-	 http.Handle("/book", bookHandler)
-	 log.Fatal(http.ListenAndServe(":8080", nil))
- }
+func decodeBookRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request bookRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
 
- func decodeBookRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	 var request bookRequest
-	 if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		 return nil, err
-	 }
-	 return request, nil
- }
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
 
- func encodeResponse(_ context.Context, w http.ResponseWriter, response interface {}) error {
-	 return json.NewEncoder(w).Encode(response)
- }
+func parse() {
+}
 
+func submit() {
+}
